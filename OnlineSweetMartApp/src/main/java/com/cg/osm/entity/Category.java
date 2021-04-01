e7eba16997df8ae2d2109f0bde8ebc42d04c8fd3
admin <admin@192.168.1.7>
2021-04-01 22:53:36 +0530

    entity and id annotation commit

--- OnlineSweetMartApp/src/main/java/com/cg/osm/entity/Category.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/entity/Category.java
@@ -1,8 +1,11 @@
 package com.cg.osm.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Category {
-
+    @Id
 	private Integer categoryId;
     private String name;
 }
