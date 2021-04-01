bc750f324a517284172bc97bd2808452992a9cba
BalaVarun123 <lsv.mail.id@gmail.com>
2021-04-01 20:37:32 +0530

    Added annotations for OrderBill

--- OnlineSweetMartApp/src/main/java/com/cg/osm/entity/OrderBill.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/entity/OrderBill.java
@@ -3,8 +3,12 @@ package com.cg.osm.entity;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class OrderBill {
-	
+	@Id
 	private Integer orderBillId;
 	private LocalDate createdDate;
 	private float totalCost;
