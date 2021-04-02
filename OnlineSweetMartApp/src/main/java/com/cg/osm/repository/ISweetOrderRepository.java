b8bc058b81b3900c5b2ad7d977a25dd27b6ddec7
BalaVarun123 <lsv.mail.id@gmail.com>
2021-04-02 17:48:44 +0530

    Added @Repository

--- OnlineSweetMartApp/src/main/java/com/cg/osm/repository/ISweetOrderRepository.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/repository/ISweetOrderRepository.java
@@ -1,10 +1,11 @@
 package com.cg.osm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.osm.entity.SweetOrder;
 
-
+@Repository
 public interface ISweetOrderRepository extends JpaRepository<SweetOrder, Integer> {
 
 
