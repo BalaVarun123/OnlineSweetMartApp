aeb6b2238c3995127d228e8c76af173af4e437df
BalaVarun123 <lsv.mail.id@gmail.com>
2021-04-02 20:19:34 +0530

    Updated IProductRepository

--- OnlineSweetMartApp/src/main/java/com/cg/osm/repository/IProductRepository.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/repository/IProductRepository.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Repository;
 import com.cg.osm.entity.Product;
 import com.cg.osm.error.ProductNotFoundException;
 @Repository
-public interface IProductRepository extends JpaRepository{
+public interface IProductRepository extends JpaRepository<Product,Integer>{
 
-	public Product addProduct(Product product);
-	public Product updateProduct(Product product) throws ProductNotFoundException;
-	public Product cancelProduct(int productId) throws ProductNotFoundException;
-	public List<Product> showAllProducts(int productId);
-	public List<Product> showAllProducts();
+	/*
+	 * public Product addProduct(Product product); public Product
+	 * updateProduct(Product product) throws ProductNotFoundException; public
+	 * Product cancelProduct(int productId) throws ProductNotFoundException; public
+	 * List<Product> showAllProducts(int productId); public List<Product>
+	 * showAllProducts();
+	 */
 	
 }
