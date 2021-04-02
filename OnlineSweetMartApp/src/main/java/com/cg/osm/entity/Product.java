b83c6c2b61639f46062dda1da761ee5d19e2f923
sai <sai@sai>
2021-04-02 11:01:49 +0530

    update entity

--- OnlineSweetMartApp/src/main/java/com/cg/osm/entity/Product.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/entity/Product.java
@@ -1,26 +1,21 @@
 package com.cg.osm.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 
 
 @Entity
-@Table(name="product")
 public class Product {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int productid;
-	@Column(name = "name")
 	private String name;
-	@Column(name = "price")
 	private double price;
-	@Column(name = "description")
 	private String description;
-	@Column(name = "availability")
 	private boolean available;
-	@Column(name = "category")
 	private String category;
 	
 	
