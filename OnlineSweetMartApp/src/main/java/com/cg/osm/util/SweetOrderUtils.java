d5739f9cab3ad5ba6ad3e86a3e8d8616d0843a23
Jerwin Richard <Jerwin Richard@LAPTOP-RQPMA2SJ>
2021-04-02 18:47:39 +0530

    sweet order module

--- OnlineSweetMartApp/src/main/java/com/cg/osm/util/SweetOrderUtils.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/util/SweetOrderUtils.java
@@ -5,42 +5,42 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import com.cg.osm.entity.OrderBill;
-import com.cg.osm.model.OrderBillDTO;
+import com.cg.osm.entity.SweetOrder;
+import com.cg.osm.model.SweetOrderDTO;
 @Component
 public class SweetOrderUtils {
-	public static List<OrderBillDTO> convertToOrderBillDtoList(List<OrderBill> list){
-		List<OrderBillDTO> dtolist = new ArrayList<OrderBillDTO>();
-		for(OrderBill orderBill : list) 
-			dtolist.add(convertToOrderBillDto(orderBill));
+	public static List<SweetOrderDTO> convertToSweetOrderDtoList(List<SweetOrder> list){
+		List<SweetOrderDTO> dtolist = new ArrayList<SweetOrderDTO>();
+		for(SweetOrder sweetOrder : list) 
+			dtolist.add(convertToSweetOrderDto(sweetOrder));
 		return dtolist;
 	}
 	
-	public static OrderBillDTO convertToOrderBillDto(OrderBill orderBill) {
-		OrderBillDTO orderBillDto = new OrderBillDTO();
-		orderBillDto.setCreatedDate(orderBill.getCreatedDate());
-		orderBillDto.setListSweetOrder(orderBill.getListSweetOrder());
-		orderBillDto.setOrderBillId(orderBill.getOrderBillId());
-		orderBillDto.setTotalCost(orderBill.getTotalCost());
-		return orderBillDto;
+	public static SweetOrderDTO convertToSweetOrderDto(SweetOrder sweetOrder) {
+		SweetOrderDTO sweetOrderDto = new SweetOrderDTO();
+		sweetOrderDto.setCreatedDate(sweetOrder.getCreatedDate());
+		sweetOrderDto.setListSweetOrder(sweetOrder.getListsweetOrder());
+		sweetOrderDto.setSweetOrderId(sweetOrder.getSweetOrderId());
+		sweetOrderDto.setTotalCost(sweetOrder.getTotalCost());
+		return sweetOrderDto;
 	}
 	
 	
 	
-	public static List<OrderBill> convertToOrderBillList(List<OrderBillDTO> dtoList){
-		List<OrderBill> list = new ArrayList<OrderBill>();
-		for(OrderBillDTO orderBillDTO : dtoList) 
-			list.add(convertToOrderBill(orderBillDTO));
+	public static List<SweetOrder> convertToSweetOrderList(List<SweetOrderDTO> dtoList){
+		List<SweetOrder> list = new ArrayList<SweetOrder>();
+		for(SweetOrderDTO sweetOrderDTO : dtoList) 
+			list.add(convertToSweetOrder(sweetOrderDTO));
 		return list;
 	}
 	
 	
-	public static OrderBill convertToOrderBill(OrderBillDTO orderBillDto) {
-		OrderBill orderBill = new OrderBill();
-		orderBill.setCreatedDate(orderBillDto.getCreatedDate());
-		orderBill.setListSweetOrder(orderBillDto.getListSweetOrder());
-		orderBill.setOrderBillId(orderBillDto.getOrderBillId());
-		orderBill.setTotalCost(orderBillDto.getTotalCost());
-		return orderBill;
+	public static SweetOrder convertToSweetOrder(SweetOrderDTO sweetOrderDto) {
+		SweetOrder sweetOrder = new SweetOrder();
+		sweetOrder.setCreatedDate(sweetOrderDto.getCreatedDate());
+		sweetOrder.setListSweetOrder(sweetOrderDto.getListSweetOrder());
+		sweetOrder.setSweetOrderId(sweetOrderDto.getSweetOrderId());
+		sweetOrder.setTotalCost(sweetOrderDto.getTotalCost());
+		return sweetOrder;
 	}
 }
