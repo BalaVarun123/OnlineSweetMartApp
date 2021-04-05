9c35a5e46a1278c6b2e1ddd2a9fc244fb6aa6611
S.JEEVETHA <S.JEEVETHA@DESKTOP-0MR8NH0>
2021-04-05 15:13:28 +0530

    controller upd

--- OnlineSweetMartApp/src/main/java/com/cg/osm/repository/IOrderBillRepository.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/repository/IOrderBillRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cg.osm.entity.OrderBill;
-import com.cg.osm.error.OrderBillNotFoundException;
-import com.cg.osm.model.OrderBillDTO;
+import com.cg.osm.error.CustomerNotFoundException;
+import com.cg.osm.model.CustomerDTO;
 
 @Repository
 public interface IOrderBillRepository extends JpaRepository<OrderBill,Integer>{
