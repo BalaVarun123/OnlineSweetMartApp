55db72b4d40a8b5fe26d59f9df7f7dc597370ca4
sai <kanakasai.1919@gmail.com>
2021-04-05 11:13:32 +0530

    mapping

--- OnlineSweetMartApp/src/main/java/com/cg/osm/entity/Product.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/entity/Product.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 
 
 
@@ -19,7 +19,7 @@ public class Product {
 	private boolean available;
 	
 	
-	@OneToOne
+	@ManyToOne
 	private Category category;
 	
 	
