db38538c48b0ef5b01c82b555e0178c97810319b
BalaVarun123 <lsv.mail.id@gmail.com>
2021-04-06 07:28:17 +0530

    Updated SweetOrder module.

--- OnlineSweetMartApp/src/main/java/com/cg/osm/entity/SweetOrder.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/entity/SweetOrder.java
@@ -2,6 +2,7 @@ package com.cg.osm.entity;
 
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,9 +36,14 @@ public class SweetOrder {
 	this.user = user;
 	this.listItems = listItems;
     this.createdDate = createdDate;
-    this.groupedProducts = groupedProducts;
-}
-
+    if (groupedProducts == null)
+    	this.groupedProducts = initiateGroupedProducts();
+    else
+    	this.groupedProducts = groupedProducts;
+	}
+	
+	
+	
 	public Integer getSweetOrderId() {
 		return sweetOrderId;
 	}
@@ -81,6 +87,24 @@ public class SweetOrder {
 	@Override
 	public String toString() {
 		   return "SweetOrder[sweetOrderId=" +sweetOrderId + ", user=" + user + ",  listItems="+ listItems +", createdDate=" + createdDate + ", groupedProducts=" + groupedProducts +"]";
+	}
+	
+	
+	public Map<Product,Long> initiateGroupedProducts() {
+		
+		Map<Product,Long> groupedProducts = new HashMap<Product, Long>();
+		if (listItems != null) {
+			Product product;
+			for (SweetItem item : listItems) {
+				product = item.getProduct();
+				if (groupedProducts.containsKey(product)) {
+					groupedProducts.put(product, groupedProducts.get(product) + 1);
+				}else {
+					groupedProducts.put(product, 1L);
+				}
+			}
+		}
+		return groupedProducts;
 	}
 	
 }
--- OnlineSweetMartApp/src/main/java/com/cg/osm/service/SweetOrderServiceImpl.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/service/SweetOrderServiceImpl.java
@@ -1,10 +1,15 @@
 package com.cg.osm.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cg.osm.entity.Product;
+import com.cg.osm.entity.SweetItem;
 import com.cg.osm.entity.SweetOrder;
 import com.cg.osm.error.SweetOrderNotFoundException;
 import com.cg.osm.model.SweetOrderDTO;
@@ -21,20 +26,38 @@ public class SweetOrderServiceImpl implements ISweetOrderService {
 
 	@Override
 	public SweetOrderDTO addSweetOrder(SweetOrder sweetOrder) {
-	
 		return  SweetOrderUtils. convertToSweetOrderDto( repository.save(sweetOrder));
 	}
 
 	@Override
 	public SweetOrderDTO updateSweetOrder(SweetOrder sweetOrder) throws SweetOrderNotFoundException {
-		
-		return null;
+		SweetOrderDTO sweetOrderDTO;
+		if (sweetOrder == null) {
+			sweetOrderDTO = null;
+		}
+		else {
+			if (repository.existsById(sweetOrder.getSweetOrderId())) {
+				sweetOrderDTO = SweetOrderUtils. convertToSweetOrderDto( repository.save(sweetOrder));
+			}
+			else {
+				throw new SweetOrderNotFoundException();
+			}
+		}
+		return sweetOrderDTO;
 	}
 
 	@Override
 	public SweetOrderDTO cancelSweetOrder(int sweetOrderId) throws SweetOrderNotFoundException {
-		
-		return null;
+		SweetOrderDTO sweetOrderDTO;
+		SweetOrder sweetOrder = repository.findById(sweetOrderId).orElse(null);
+		if (sweetOrder != null) {
+			repository.delete(sweetOrder);
+			sweetOrderDTO = SweetOrderUtils.convertToSweetOrderDto(sweetOrder);
+		} else {
+			throw new SweetOrderNotFoundException();
+		}
+
+		return sweetOrderDTO;
 	}
 
 	@Override
@@ -45,8 +68,43 @@ public class SweetOrderServiceImpl implements ISweetOrderService {
 
 	@Override
 	public double calculateTotalCost(int sweetOrderId) {
+		double sum = 0.0;
+		Long count;
+		SweetOrder sweetOrder = repository.findById(sweetOrderId).orElse(null);
+		if (sweetOrder != null) {
+			Map<Product, Long> groupedProducts = sweetOrder.getGroupedProducts();
+			if (groupedProducts != null) {
+				Set<Product> keys = groupedProducts.keySet();
+				for (Product product : keys) {
+					count =  groupedProducts.get(product);
+					if (count != null) {
+						sum += (product.getPrice() * count);
+					}
+				}
+			}
+			
+		}
+		return sum;
+	}
 	
-		return 0;
+	public static boolean vaidateGroupedProducts(SweetOrder sweetOrder) {
+		boolean result;
+		Map<Product,Long> groupedProducts = null;
+		groupedProducts = sweetOrder.getGroupedProducts();
+		
+		if (groupedProducts == null) {
+			result = false;
+		}
+		else {
+			Map<Product,Long> correctGroupedProducts = sweetOrder.initiateGroupedProducts();
+			if (groupedProducts.equals(correctGroupedProducts)) {
+				result = true;
+			}
+			else {
+				result = false;
+			}
+		}
+		return result;
 	}
 
 }
