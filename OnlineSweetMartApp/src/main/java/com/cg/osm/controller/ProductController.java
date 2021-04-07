b1dbdf15c01ffc022cc2f1bf04228135500c4723
admin <admin@192.168.1.7>
2021-04-07 23:45:50 +0530

    update

---
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/controller/ProductController.java
@@ -0,0 +1,89 @@
+package com.cg.osm.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.osm.entity.Product;
+import com.cg.osm.error.ProductNotFoundException;
+import com.cg.osm.model.ProductDTO;
+import com.cg.osm.service.IProductService;
+import com.cg.osm.service.ProductServiceImpl;
+
+@RestController
+@RequestMapping("/api/osm")
+public class ProductController {
+	
+	@Autowired
+	IProductService service;
+	
+	
+	
+	
+	@PostMapping(value = "/product/add", produces = "application/json", consumes  = "application/json")
+	public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product) throws ProductNotFoundException {
+		ResponseEntity<ProductDTO> productResponse;
+		if (ProductServiceImpl.validateProduct(product)) {
+			
+			
+			
+			ProductDTO result=service.addProduct(product);
+			productResponse = new ResponseEntity<ProductDTO>(result,HttpStatus.ACCEPTED);
+			System.out.println("Product Added");
+
+		} else
+
+			throw new ProductNotFoundException("Please enter valid product details");
+
+		return productResponse;
+		
+	}
+	
+	
+	@PutMapping(value = "/product/update", produces = "application/json",consumes  = "application/json")
+	public ResponseEntity<ProductDTO> updateProduct(@RequestBody Product product) throws ProductNotFoundException {
+		ResponseEntity<ProductDTO> productResponse;
+		if (ProductServiceImpl.validateProduct(product) && ProductServiceImpl.validateProductId(product)) {
+			
+			ProductDTO result=service.updateProduct(product);
+			productResponse = new ResponseEntity<ProductDTO>(result, HttpStatus.ACCEPTED);
+			System.out.println("Product Updated");
+
+		} else
+
+			throw new ProductNotFoundException("Please enter valid product details");
+
+		return productResponse;
+	}
+	
+	@DeleteMapping(value = "/product/cancel/{productid}", produces = "application/json")
+	public void cancelProduct(@PathVariable("productid") int productid) throws ProductNotFoundException{
+		service.cancelProduct(productid);
+		
+	}
+	
+	@GetMapping(value = "/product/show-all", produces = "application/json")
+	public List<ProductDTO> showAllProducts(){
+		return service.showAllProducts();
+	} 
+	
+	@GetMapping(value = "/product/show-by-id/{product}", produces = "application/json")
+	public ProductDTO showAllProductDTO(@PathVariable("productid") int productid) throws ProductNotFoundException{
+		return service.showAllProducts(productid);
+	
+	}
+	
+
+	
+	
+}
