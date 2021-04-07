98c3f570798d1a5633da482ea7fce5d0d3843fff
admin <admin@192.168.1.7>
2021-04-07 23:41:36 +0530

    Multi line comments

--- OnlineSweetMartApp/src/main/java/com/cg/osm/service/CategoryServiceImpl.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/service/CategoryServiceImpl.java
@@ -10,17 +10,37 @@ import com.cg.osm.error.CategoryNotFoundException;
 import com.cg.osm.model.CategoryDTO;
 import com.cg.osm.repository.ICategoryRepository;
 import com.cg.osm.util.CategoryUtils;
+
+/*
+ * Author : RAKSHA R
+ * Version : 1.0
+ * Date : 02-04-2021
+ * Description : This is Category Service Layer
+*/
+
 @Service
 public class CategoryServiceImpl implements ICategoryService {
 
 	@Autowired
 	private ICategoryRepository repo;
 	
+	/*
+	 * Description : This method Adds new Category
+	 * Input Param : Category Object 
+	 * Return Value : CategoryDTO Object 
+	*/
 	@Override
-	public CategoryDTO addCategory(Category category) {
+	public CategoryDTO addCategory(Category category) 
+	{
 		Category ctgy = repo.save(category);
 		return CategoryUtils.convertToCategoryDto(ctgy);
 	}
+	
+	/*
+	 * Description : This method updates Category
+	 * Input Param : Category Object 
+	 * Return Value : CategoryDTO Object 
+	*/
 
 	@Override
 	public CategoryDTO updateCategory(Category category) throws CategoryNotFoundException 
@@ -35,6 +55,12 @@ public class CategoryServiceImpl implements ICategoryService {
 		else
 		return CategoryUtils.convertToCategoryDto(repo.save(category));
 	}
+	
+	/*
+	 * Description : This method deletes Category
+	 * Input Param : int 
+	 * Return Value : CategoryDTO Object 
+	*/
 
 	@Override
 	public CategoryDTO cancelCategory(int categoryId) throws CategoryNotFoundException {
@@ -49,6 +75,11 @@ public class CategoryServiceImpl implements ICategoryService {
 			return CategoryUtils.convertToCategoryDto(existingCategory);
 		}
 	}
+	/*
+	 * Description : This method Shows existing Categories
+	 * Return Value : CategoryDTO Object 
+	 * Exception : CategoryNotFoundException
+	 */
 
 	@Override
 	public List<CategoryDTO> showAllCategorys() {
@@ -56,6 +87,12 @@ public class CategoryServiceImpl implements ICategoryService {
 		
 		return CategoryUtils.convertToCategoryDtoList(list);
 	}
+	
+	/*
+	 * Description : This method calculates the total cost for that cateory id
+	 * Input Param : int
+	 * Return Value : double
+	  */
 
 	@Override
 	public double calculateTotalCost(int categoryId) 
@@ -63,6 +100,8 @@ public class CategoryServiceImpl implements ICategoryService {
 		return repo.calculateTotalCost(categoryId);
 	}
 	
+	
+	//VALIDATIONS
 	public static boolean validateCategoryId(Category category) throws CategoryNotFoundException
 	{
 		boolean flag=false;
