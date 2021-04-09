c0e230824b9a16a52fd76b3f96abfede1b626bb0
BalaVarun123 <lsv.mail.id@gmail.com>
2021-04-09 07:12:50 +0530

    Changed OrderBillServiceImpl and it's test

--- OnlineSweetMartApp/src/main/java/com/cg/osm/service/OrderBillServiceImpl.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/service/OrderBillServiceImpl.java
@@ -68,49 +68,53 @@ public class OrderBillServiceImpl implements IOrderBillService{
 	}
 	
 	
-	public static boolean validateOrder(OrderBill orderBill) {
-		boolean flag;
-		if (orderBill == null  ) {
-			flag = false;
-		}
-		else if (!(validateOrderBillCreatedDate(orderBill) && validateOrderBillListSweetOrder(orderBill) &&  validateOrderBillId(orderBill) && validateOrderBillTotalCost(orderBill))) {
-			flag = false;
-		}
-		else {
-			flag = true;
-		}
-		return flag;
-	}
+
 	
 
 	public static boolean validateOrderBillCreatedDate(OrderBill orderBill) {
 		boolean flag = true;
-		if (orderBill.getCreatedDate() == null || orderBill.getCreatedDate().isAfter(LocalDate.now()))
+		if (orderBill == null || orderBill.getCreatedDate() == null || orderBill.getCreatedDate().isAfter(LocalDate.now()))
 			flag = false;
 		return flag;
 	}
 	
 	public static boolean validateOrderBillListSweetOrder(OrderBill orderBill) {
 		boolean flag = true;
-		List<SweetOrder> listSweetOrder = orderBill.getListSweetOrder();
-		if (listSweetOrder == null || listSweetOrder.size() == 0)
+		if (orderBill== null) {
 			flag = false;
+		}
+		else {
+			List<SweetOrder> listSweetOrder = orderBill.getListSweetOrder();
+			if (listSweetOrder == null || listSweetOrder.size() == 0)
+				flag = false;
+		}
+		
 		return flag;
 	}
 	public static boolean validateOrderBillId(OrderBill orderBill) {
 		boolean flag = true;
-		Integer id = orderBill.getOrderBillId();
-		//OrderBillServiceImpl service1 = new OrderBillServiceImpl();
-		if (id == null|| id < 0 )
+		if (orderBill == null) {
 			flag = false;
+		}
+		else {
+			Integer id = orderBill.getOrderBillId();
+			if (id == null|| id < 0 )
+				flag = false;
+		}
+		
 		return flag;
 	}
 	
 	public static boolean validateOrderBillTotalCost(OrderBill orderBill) {
 		boolean flag = true;
-		float totalCost = orderBill.getTotalCost();
-		if ( totalCost < 0 || Float.isNaN(totalCost))
+		if (orderBill == null) {
 			flag = false;
+		}
+		else {
+			float totalCost = orderBill.getTotalCost();
+			if ( totalCost < 0 || Float.isNaN(totalCost))
+				flag = false;
+		}
 		return flag;
 	}
 
