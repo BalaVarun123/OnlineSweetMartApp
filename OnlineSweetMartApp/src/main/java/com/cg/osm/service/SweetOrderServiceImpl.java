bbd963b2a2178b6b0836da014b38ee249f567182
BalaVarun123 <lsv.mail.id@gmail.com>
2021-04-11 19:52:00 +0530

    Updated SweetOrderService Impl with Logger and comments.

--- OnlineSweetMartApp/src/main/java/com/cg/osm/service/SweetOrderServiceImpl.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/service/SweetOrderServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,77 +20,132 @@ import com.cg.osm.repository.ISweetOrderRepository;
 import com.cg.osm.util.SweetOrderUtils;
 
 @Service
-
+/*
+ * Author      : BALASUBRAMANIAN S
+ * Version     : 1.0
+ * Date        : 05-04-2021
+ * Description : Implementation for ISweetOrderService
+*/
 public class SweetOrderServiceImpl implements ISweetOrderService {
 	@Autowired
 	ISweetOrderRepository repository;
 
+	static final Logger LOGGER = LoggerFactory.getLogger(CartServiceImp.class);
+	
+	/*
+	 * Description     : This method adds a new SweetOrder record.
+	 * Input Parameter : SweetOrder Object 
+	 * Return Value    : SweetOrderDTO Object 
+	*/
 	@Override
 	public SweetOrderDTO addSweetOrder(SweetOrder sweetOrder) {
+		LOGGER.info("addSweetOrder service method is initiated.");
 		SweetOrderDTO sweetOrderDTO;
 		if (sweetOrder ==null) {
+			LOGGER.error("sweetOrder is null.");
 			sweetOrderDTO = null;
 		}
 		else {
 			sweetOrderDTO = SweetOrderUtils.convertToSweetOrderDto(repository.save(sweetOrder));
 		}
+		LOGGER.info("addSweetOrder service method is terminated.");
 		return sweetOrderDTO;
 	}
-
+	
+	
+	
+	/*
+	 * Description     : This method updates an existing SweetOrder record.
+	 * Input Parameter : SweetOrder Object 
+	 * Return Value    : SweetOrderDTO Object 
+	 * Exception       : SweetOrderNotFoundException
+	*/
 	@Override
 	public SweetOrderDTO updateSweetOrder(SweetOrder sweetOrder) throws SweetOrderNotFoundException {
+		LOGGER.info("updateSweetOrder service method is initiated.");
 		SweetOrderDTO sweetOrderDTO;
 		if (sweetOrder == null) {
+			LOGGER.error("sweetOrder is null.");
 			sweetOrderDTO = null;
 		} else {
 			if (repository.existsById(sweetOrder.getSweetOrderId())) {
 				sweetOrderDTO = SweetOrderUtils.convertToSweetOrderDto(repository.save(sweetOrder));
 			} else {
+				LOGGER.error("Invalid sweetOrderId.");
 				throw new SweetOrderNotFoundException("Invalid sweetOrderId.");
 			}
 		}
+		LOGGER.info("updateSweetOrder service method is terminated.");
 		return sweetOrderDTO;
 	}
 
+	
+	/*
+	 * Description     : This method removes an existing SweetOrder record.
+	 * Input Parameter : int sweetOrderId 
+	 * Return Value    : SweetOrderDTO Object 
+	 * Exception       : SweetOrderNotFoundException
+	*/
 	@Override
 	public SweetOrderDTO cancelSweetOrder(int sweetOrderId) throws SweetOrderNotFoundException {
+		LOGGER.info("cancelSweetOrder service method is initiated.");
 		SweetOrderDTO sweetOrderDTO;
 		SweetOrder sweetOrder = repository.findById(sweetOrderId).orElse(null);
 		if (sweetOrder != null) {
 			repository.delete(sweetOrder);
 			sweetOrderDTO = SweetOrderUtils.convertToSweetOrderDto(sweetOrder);
 		} else {
+			LOGGER.error("Invalid sweetOrderId.");
 			throw new SweetOrderNotFoundException("Invalid sweetOrderId.");
 		}
-
+		LOGGER.info("cancelSweetOrder service method is terminated.");
 		return sweetOrderDTO;
 	}
 	
 	
 	
-	
+	/*
+	 * Description     : This retrieves a single SweetOrder instance by sweetOrderId.
+	 * Input Parameter : int sweetOrderId
+	 * Return Value    : SweetOrderDTO Object 
+	 * Exception       : SweetOrderNotFoundException
+	*/
 	@Override
 	public SweetOrderDTO showSweetOrder(int sweetOrderId) throws SweetOrderNotFoundException {
+		LOGGER.info("showSweetOrder service method is initiated.");
 		SweetOrderDTO sweetOrderDTO;
 		SweetOrder sweetOrder = repository.findById(sweetOrderId).orElse(null);
 		if (sweetOrder != null) {
 			sweetOrderDTO = SweetOrderUtils.convertToSweetOrderDto(sweetOrder);
 		} else {
+			LOGGER.error("Invalid sweetOrderId.");
 			throw new SweetOrderNotFoundException("Invalid sweetOrderId.");
 		}
-
+		LOGGER.info("showSweetOrder service method is terminated.");
 		return sweetOrderDTO;
 	}
 
+	/*
+	 * Description     : This method retrieves all SweetOrder records.
+	 * Return Value    : List<SweetOrderDTO> Object 
+	*/
 	@Override
 	public List<SweetOrderDTO> showAllSweetOrders() {
-
+		LOGGER.info("showAllSweetOrders service method is executed.");
 		return SweetOrderUtils.convertToSweetOrderDtoList(repository.findAll());
 	}
 	
 	
+	/*
+	 * Description     : This method returns the total cost of all sweet items in this order.
+	 * Input Parameter : int sweetOrderId
+	 * Return Value    : double total cost of sweet items.
+	 * Exception       : SweetOrderNotFoundException
+	*/
+	
 	@Override
 	public double calculateTotalCost(int sweetOrderId) throws SweetOrderNotFoundException {
+		LOGGER.info("calculateTotalCost service method is initiated.");
 		double total = 0;
 		SweetOrder sweetOrder = repository.findById(sweetOrderId).orElse(null);
 		if (sweetOrder != null) {
@@ -97,36 +154,15 @@ public class SweetOrderServiceImpl implements ISweetOrderService {
 				total += item.getProduct().getPrice();
 			}
 		} else {
+			LOGGER.error("Invalid sweetOrderId.");
 			throw new SweetOrderNotFoundException("Invalid sweetOrderId.");
 		}
-
+		LOGGER.info("calculateTotalCost service method is terminated.");
 		return total;
 	}
-
-
-	/*
-	 * @Override public double calculateTotalCost(int sweetOrderId) { double sum =
-	 * 0.0; Long count; SweetOrder sweetOrder =
-	 * repository.findById(sweetOrderId).orElse(null); if (sweetOrder != null) {
-	 * Map<Product, Long> groupedProducts = sweetOrder.getGroupedProducts(); if
-	 * (groupedProducts != null) { Set<Product> keys = groupedProducts.keySet(); for
-	 * (Product product : keys) { count = groupedProducts.get(product); if (count !=
-	 * null) { sum += (product.getPrice() * count); } } }
-	 * 
-	 * } return sum; }
-	 */
-
-	/*
-	 * public static boolean vaidateGroupedProducts(SweetOrder sweetOrder) { boolean
-	 * result; Map<Product, Long> groupedProducts = null; groupedProducts =
-	 * sweetOrder.getGroupedProducts();
-	 * 
-	 * if (groupedProducts == null) { result = false; } else { Map<Product, Long>
-	 * correctGroupedProducts = sweetOrder.initiateGroupedProducts(); if
-	 * (groupedProducts.equals(correctGroupedProducts)) { result = true; } else {
-	 * result = false; } } return result; }
-	 */
-
+	
+	
+	//Validation for sweetOrderId field of a SweetOrder instance.
 	public static boolean validateSweetOrderId(SweetOrder sweetOrder) {
 		boolean result;
 		if (sweetOrder == null) {
@@ -139,13 +175,17 @@ public class SweetOrderServiceImpl implements ISweetOrderService {
 				result = false;
 			}
 		}
+		LOGGER.info("validateSweetOrderId is executed.");
 		return result;
 	}
 
+	//Validation for user field of a SweetOrder instance.
 	public static boolean validateUser(SweetOrder sweetOrder) {
+		LOGGER.info("validateUser is executed.");
 		return (sweetOrder != null && sweetOrder.getUser() != null);
 	}
 
+	//Validation for listItems field of a SweetOrder instance.
 	public static boolean validateListItems(SweetOrder sweetOrder) {
 		boolean result = false;
 		if (sweetOrder != null) {
@@ -154,9 +194,13 @@ public class SweetOrderServiceImpl implements ISweetOrderService {
 				result = true;
 			}
 		}
+		LOGGER.info("validateListItems is executed.");
 		return result;
 	}
 
+	
+	
+	//Validation for createdDate field of a SweetOrder instance.
 	public static boolean validateCreatedDate(SweetOrder sweetOrder) {
 		boolean result = false;
 		if (sweetOrder != null) {
@@ -165,7 +209,7 @@ public class SweetOrderServiceImpl implements ISweetOrderService {
 				result = true;
 			}
 		}
-
+		LOGGER.info("validateCreatedDate is executed.");
 		return result;
 	}
 
