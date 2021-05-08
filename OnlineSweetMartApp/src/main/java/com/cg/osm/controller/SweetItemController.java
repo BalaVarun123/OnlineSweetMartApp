7254825934850c0e520ee1d94cc8b7acc5af59cf
Annie <anniehepzhibha@gmail.com>
2021-05-08 10:10:23 +0530

    added crossorigin

--- OnlineSweetMartApp/src/main/java/com/cg/osm/controller/SweetItemController.java
+++ OnlineSweetMartApp/src/main/java/com/cg/osm/controller/SweetItemController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +34,7 @@ import com.cg.osm.util.SweetOrderUtils;
  * Date : 05-04-2021
  * Description : This is SweetItem Controller
 */
-
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/osm")
 
